dca3bd73ddf7317cee447b43e12ecffe7ad7f866
liyiyang <li.yiyang@lezyo.com>
2016-02-17 15:49:35 +0800

    fix

--- ZTree.php
+++ ZTree.php
@@ -1,32 +1,52 @@
 <?php
 
-namespace liyuze\ztree
+namespace liyuze\ztree;
 
 use yii\base\Widget;
 
 class ZTree extends Widget
 {
 
-    private $_setting = '[]';
+    private $_setting = '{}';
 
     private $_nodes = '[]';
+
+    private $id;
 
 
     public function init()
     {
-        if (is_null($this->_id))
-            $this->getId();
+        if (is_null($this->id))
+            $this->id = $this->getId();
     }
 
     public function run()
     {
-        echo '<ul id="'.$this->_id.'" class="ztree"></ul>';
+        echo '<ul id="'.$this->id.'" class="ztree"></ul>';
 
         $view = $this->getView();
 
-        ZTreeAsset::register($view);
+        $t = new ZTreeAsset;
+        $t->css = ['css/awesomeStyle/awesome.css'];
+        $t::register($view);
 
-        $js = '$.fn.zTree.init($("#'.$this->_id.'"), $this->_setting, $this->_nodes);';
+        $js = '$.fn.zTree.init($("#'.$this->id.'"), '.$this->_setting.', '.$this->_nodes.');';
         $view->registerJs($js);
+    }
+
+    public function setSetting($value)
+    {
+        if (is_array($value))
+            $value = json_encode($value, JSON_UNESCAPED_UNICODE);
+
+        $this->_setting = $value;
+    }
+
+    public function setNodes($value)
+    {
+        if (is_array($value))
+            $value = json_encode($value, JSON_UNESCAPED_UNICODE);
+
+        $this->_nodes = $value;
     }
 }
--- ZTreeAsset.php
+++ ZTreeAsset.php
@@ -13,25 +13,30 @@ use yii\web\AssetBundle;
 class ZTreeAsset extends AssetBundle
 {
     public $sourcePath = '@liyuze/ztree/assets';
+
     public $js = [
-        'jquery.ztree.all-3.5.min.js',      //完整包
-        // 'jquery.ztree.all-3.5.js',
-        // 'jquery.ztree.core-3.5.min.js',     //核心包
-        // 'jquery.ztree.core-3.5.js',
-        // 'jquery.ztree.excheck-3.5.min.js',  //excheck 扩展包
-        // 'jquery.ztree.excheck-3.5.js',
-        // 'jquery.ztree.exedit-3.5.min.js',   //exedit 扩展包
-        // 'jquery.ztree.exedit-3.5.js',
-        // 'jquery.ztree.exhide-3.5.min.js',   //exhide 扩展包
-        // 'jquery.ztree.exhide-3.5.js',
+        'js/jquery.ztree.all-3.5.min.js',      //完整包
+        // 'js/jquery.ztree.all-3.5.js',
+        // 'js/jquery.ztree.core-3.5.min.js',     //核心包
+        // 'js/jquery.ztree.core-3.5.js',
+        // 'js/jquery.ztree.excheck-3.5.min.js',  //excheck 扩展包
+        // 'js/jquery.ztree.excheck-3.5.js',
+        // 'js/jquery.ztree.exedit-3.5.min.js',   //exedit 扩展包
+        // 'js/jquery.ztree.exedit-3.5.js',
+        // 'js/jquery.ztree.exhide-3.5.min.js',   //exhide 扩展包
+        // 'js/jquery.ztree.exhide-3.5.js',
+        // 'js/jquery-1.4.4.min.js',
     ];
+
     public $css = [
         #皮肤样式
-        'zTreeStyle/zTreeStyle.css',
-        // 'awesomeStyle/awesomeStyle.css',
-        // 'metroStyle/metroStyle.css',
+        'css/zTreeStyle/zTreeStyle.css',
+        // 'css/awesomeStyle/awesome.css',
+        // 'css/metroStyle/metroStyle.css',
     ];
+
     public $depends = [
         'yii\web\JqueryAsset'
     ];
+
 }
